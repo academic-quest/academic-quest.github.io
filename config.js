0c744d71bd22729e7da96a00019af3f7663dafb8
MyoMyint-rsu <myomyint.k67@rsu.ac.th>
2025-09-25 19:19:32 +0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyB-vArRoYuCqzIWT_b7xPV1Lt6Ev4T3Bsc",
-  authDomain: "academic-quest.firebaseapp.com",
-  projectId: "academic-quest",
-  storageBucket: "academic-quest.firebasestorage.app",
-  messagingSenderId: "44540247560",
-  appId: "1:44540247560:web:96e1d5bc34baf97e3b1bbd"
-};
